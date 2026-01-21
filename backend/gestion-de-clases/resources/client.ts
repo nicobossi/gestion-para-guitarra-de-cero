83f0b7877736878af30f8f9723ad5bb1fa725db5
Nicolás Bossi <nbossi@hotmail.com>
2026-01-21 19:29:31 -0300

    se cambió la ruta de la variable de entorno y se leyó la url de la base de datos al instanciar el cliente de prisma

--- backend/gestion-de-clases/resources/client.ts
+++ backend/gestion-de-clases/resources/client.ts
@@ -1,7 +1,9 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from 'resources/generated/prisma/client';
+import dotenv from "dotenv";
+import path from 'path';
 
-
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 const connectionString = process.env.DATABASE_URL;
 const adapter = new PrismaPg({ connectionString });
 const client = new PrismaClient({ adapter });
