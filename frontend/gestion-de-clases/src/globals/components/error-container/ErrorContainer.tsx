a3a9f9797138eb7f8985afed34510877faf81418
Nicolás Bossi <nbossi@hotmail.com>
2026-02-11 16:36:33 -0300

    Se modificó el estilo del contenedor de error y se agregaron animaciones

--- frontend/gestion-de-clases/src/globals/components/error-container/ErrorContainer.tsx
+++ frontend/gestion-de-clases/src/globals/components/error-container/ErrorContainer.tsx
@@ -1,18 +1,27 @@
-import GenericContainer from '../generic-container/GenericContainer';
-import TitleContainer from '../title-container/TitleContainer';
+import { useState } from 'react'
 import type { ErrorContainerProps } from './error-container'
 import './error-container.css'
 
 const ErrorContainer = ({content, onSubmit} : ErrorContainerProps) => {
+
+    const [isClick, setClick] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setClick(true);
+        setTimeout(onSubmit, 400)
+    }
     
     return (
-        <GenericContainer>
-            <TitleContainer title = "Ingreso Fallido" />
-            <div className = 'error-content'>
-                <p>{content}</p>
-                <button type="button" onClick = {onSubmit}>Volver a intentar</button>
-            </div>
-        </GenericContainer>
+        <div className = {`error-container ${isClick && "error-container_disable"}`}>
+            <section className = 'error-background'>
+                <h2>¡Oh no!</h2>
+            </section>
+            <section className = 'error-content'>
+                <h3>Ingreso Fallido</h3>
+                <p className = 'error-content_description'>{content}</p>
+                <button type="button" onClick = {handleClick}>Volver a intentar</button>
+            </section>
+        </div>
     )
 }
 
