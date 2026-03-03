7b238da0f7afd7846ab07637f544b6bf1e89cbf3
Nicolás Bossi <nbossi@hotmail.com>
2026-03-03 14:44:51 -0300

    Se uso el componente de React Router para navegar a la otra página

--- frontend/gestion-de-clases/src/globals/components/nav-item/NavItem.tsx
+++ frontend/gestion-de-clases/src/globals/components/nav-item/NavItem.tsx
@@ -1,29 +1,58 @@
 import { Link } from "react-router";
 import type { NavItemProps } from "./nav-item";
 import { useState } from "react";
-
+import './nav-item.css';
+import type { PageData } from "../side-bar/types/page-data";
 
 const NaveItem = ({page} : NavItemProps) => {
 
     const [isVisible, setIsVisible] = useState<boolean>(false);
 
-    const changeVisible = () => setIsVisible(value => value!);
+    const changeVisible = () => setIsVisible(prevIsVisible => !prevIsVisible);
     
     return (
         <div className="nav-item">
-            <section className="nav-item_desciption" onClick={changeVisible}>
-                <p>{page.pageName}</p>
-                <figure>
-                    <img src="" alt="" />
-                </figure>
-            </section>
-            {isVisible && <section className="nav-item_links">
-                {page.links.map(link => 
-                    <Link to={link.linkName} />
-                )}
-            </section>
-            }
+            <NavDescription 
+                pageName={page.pageName} 
+                changeVisible={changeVisible} />
+            {isVisible && <LinksList page={page} />}
         </div>
+    )
+}
+
+type NavDescriptionProps = {
+    pageName : string,
+    changeVisible : () => void
+}
+
+const NavDescription = ({pageName, changeVisible} : NavDescriptionProps) => {
+
+    return (
+        <section className="nav-item_desciption" onClick={changeVisible}>
+            <p>{pageName}</p>
+            <figure>
+                    
+            </figure>
+        </section>
+    )
+}
+
+type LinksListProps = {
+    page : PageData
+}
+
+const LinksList = ({page} : LinksListProps) => {
+
+    return (
+        <section className="nav-item_links">
+            {page.links.map(link => 
+                <Link 
+                    to={`/${link.link}`} 
+                    key={page.pageName}>
+                {link.linkName}
+                </Link>
+            )}
+        </section>
     )
 }
 
--- frontend/gestion-de-clases/src/globals/components/navegator-pages/NavegatorPages.tsx
+++ frontend/gestion-de-clases/src/globals/components/navegator-pages/NavegatorPages.tsx
@@ -1,13 +1,16 @@
 import NavItem from "../nav-item/NavItem";
 import type { NavegatorPageProps } from "./navegator-pages";
-
+import './navegator-pages.css';
 
 const NavegatorPage = ({pages} : NavegatorPageProps) => {
 
     return (
         <section className="navegator-container">
             {pages.map(page => 
-                <NavItem page={page} />
+                <NavItem 
+                    page={page} 
+                    key={page.pageName}
+                />
             )}
         </section>
     )
