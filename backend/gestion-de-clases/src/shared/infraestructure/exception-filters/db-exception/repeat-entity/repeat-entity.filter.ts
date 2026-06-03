a8cffab72737ffe5f7ad54b9a5d184bdc2923409
Nicolás Bossi <nbossi@hotmail.com>
2026-06-02 23:39:10 -0300

    Se agregó el filtro de entidad repetida a la jerarquia de filtros para la base de datos

--- backend/gestion-de-clases/src/shared/infraestructure/exception-filters/db-exception/repeat-entity/repeat-entity.filter.ts
+++ backend/gestion-de-clases/src/shared/infraestructure/exception-filters/db-exception/repeat-entity/repeat-entity.filter.ts
@@ -1,18 +1,15 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Catch } from '@nestjs/common';
 import { RepeatEntityException } from '../../../persistence/errors/repeat-entity-exception';
+import { FilterExceptionDbData } from '../response-data';
+import { DbFilterException } from '../db-filter-exception';
 
 @Catch(RepeatEntityException)
-export class RepeatEntityFilter implements ExceptionFilter {
-    catch(_: RepeatEntityException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const request = ctx.getRequest<Request>();
-        const response = ctx.getResponse<Response>();
-        response.status(400).json({
+export class RepeatEntityFilter extends DbFilterException<RepeatEntityException> {
+    protected responseData(): FilterExceptionDbData {
+        return {
+            status: 400,
             message:
                 'Existen datos ingresados que ya se encuentran registrados',
-            timestamp: new Date().toISOString(),
-            path: request.url,
-        });
+        };
     }
 }
