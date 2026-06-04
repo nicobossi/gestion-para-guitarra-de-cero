d5b06429e04b3b2939440f8823632ff4de0c1a4d
Nicolás Bossi <nbossi@hotmail.com>
2026-06-04 14:01:35 -0300

    Se agregó un test para la funcionalidad de ingreso de estudiante

--- backend/gestion-de-clases/src/features/student/infraestructure/persistence/student.repository.spec.ts
+++ backend/gestion-de-clases/src/features/student/infraestructure/persistence/student.repository.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentRepository } from './student.repository';
 import { SqlClient } from '../../../../shared/infraestructure/persistence/sql/prisma.service';
+import { Student } from '../../domain/student';
 
 describe('StudentRepository', () => {
     let repository: StudentRepository;
+    let student: Student;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -11,9 +13,15 @@ describe('StudentRepository', () => {
         }).compile();
 
         repository = module.get<StudentRepository>(StudentRepository);
+        student = new Student('John', 'Dee', 1234567890, new Date(), 'Michael');
     });
 
     it('should be defined', () => {
         expect(repository).toBeDefined();
+    });
+
+    it('should income a student', async () => {
+        const createdStudent = await repository.income(student);
+        expect(createdStudent.getId).toBeDefined();
     });
 });
