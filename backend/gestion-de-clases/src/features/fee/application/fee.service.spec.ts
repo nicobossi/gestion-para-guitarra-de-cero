a5e96324cec5e5dae14ee5b431a779159d775fd7
Nicolás Bossi <nbossi@hotmail.com>
2026-06-10 18:01:44 -0300

    Se agregaron tests para los servicios de la cuota y del estudiante

--- backend/gestion-de-clases/src/features/fee/application/fee.service.spec.ts
+++ backend/gestion-de-clases/src/features/fee/application/fee.service.spec.ts
@@ -2,9 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { FeeService } from './fee.service';
 import { FeeRepository } from '../infraestructure/persistence/fee.repository';
 import { SqlClient } from '../../../shared/infraestructure/persistence/sql/prisma.service';
+import { StartedPostgreSqlContainer } from '@testcontainers/postgresql';
+import {
+    createDatabaseToContainer,
+    createSqlContainer,
+} from '../../../../test/containers/sql';
+import { clearSqlContainer } from '../../../../test/containers/tear-down';
+import { RepeatAmountException } from '../domain/repeat-amount-exception';
+import { Fee } from '../domain/fee';
+import { PaymentLapse } from '../domain/payment-lapse';
 
 describe('FeeService', () => {
     let service: FeeService;
+    let container: StartedPostgreSqlContainer;
+    let fee: Fee;
+
+    beforeAll(async () => {
+        container = await createSqlContainer().start();
+        createDatabaseToContainer(container);
+    });
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +28,31 @@ describe('FeeService', () => {
         }).compile();
 
         service = module.get<FeeService>(FeeService);
+        fee = new Fee(500, PaymentLapse.MONTHLY, new Date());
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('should add a fee', async () => {
+        const addedFee = await service.add(fee);
+        expect(addedFee.getId).toBeDefined();
+    });
+
+    it('should throw the exception repeat amount exception if the amount already exists', async () => {
+        const addedFee = await service.add(fee);
+        const studentWithRepeatPhone = async () => await service.add(addedFee);
+        await expect(studentWithRepeatPhone).rejects.toBeInstanceOf(
+            RepeatAmountException,
+        );
+    });
+
+    afterEach(async () => {
+        await clearSqlContainer();
+    });
+
+    afterAll(async () => {
+        await container.stop();
     });
 });
--- backend/gestion-de-clases/src/features/student/application/student.service.spec.ts
+++ backend/gestion-de-clases/src/features/student/application/student.service.spec.ts
@@ -2,19 +2,57 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StudentService } from './student.service';
 import { StudentRepository } from '../infraestructure/persistence/student.repository';
 import { SqlClient } from '../../../shared/infraestructure/persistence/sql/prisma.service';
+import { StartedPostgreSqlContainer } from '@testcontainers/postgresql';
+import {
+    createDatabaseToContainer,
+    createSqlContainer,
+} from '../../../../test/containers/sql';
+import { clearSqlContainer } from '../../../../test/containers/tear-down';
+import { Student } from '../domain/student';
+import { RepeatPhoneException } from '../domain/repeat-phone-exception';
 
 describe('StudentService', () => {
+    let student: Student;
     let service: StudentService;
+    let container: StartedPostgreSqlContainer;
+
+    beforeAll(async () => {
+        container = await createSqlContainer().start();
+        createDatabaseToContainer(container);
+    });
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [StudentService, StudentRepository, SqlClient],
         }).compile();
 
+        student = new Student('Carlos', 'Brockhampton', 1234567891, new Date());
         service = module.get<StudentService>(StudentService);
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('should incoming a student', async () => {
+        const incomerStudent = await service.income(student);
+        expect(incomerStudent.getId).toBeDefined();
+    });
+
+    it('should throw the exception repeat phone exception if the phone already exists', async () => {
+        const incomerStudent = await service.income(student);
+        const studentWithRepeatPhone = async () =>
+            await service.income(incomerStudent);
+        await expect(studentWithRepeatPhone).rejects.toBeInstanceOf(
+            RepeatPhoneException,
+        );
+    });
+
+    afterAll(async () => {
+        await container.stop();
+    });
+
+    afterEach(async () => {
+        await clearSqlContainer();
     });
 });
