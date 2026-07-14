94fff68a52b419df881c5847343b0029af55bc43
Nicolás Bossi <nbossi@hotmail.com>
2026-07-14 12:50:47 -0300

    Se hizo una validación en la creación del cliente de prisma

--- backend/gestion-de-clases/src/shared/infraestructure/persistence/sql/prisma.service.ts
+++ backend/gestion-de-clases/src/shared/infraestructure/persistence/sql/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../../../../generated/prisma/client';
+import { CredentialsError } from '../errors/credentials-error';
 
 @Injectable()
 export class SqlClient
@@ -8,6 +9,9 @@ export class SqlClient
     implements OnModuleInit, OnModuleDestroy
 {
     constructor() {
+        if (!process.env.DATABASE_URL) {
+            throw new CredentialsError();
+        }
         const adapter = new PrismaPg({
             connectionString: `${process.env.DATABASE_URL}`,
         });
