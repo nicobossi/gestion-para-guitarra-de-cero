dcd234ae2694da432dbbb832403251761fa8707f
Nicolás Bossi <nbossi@hotmail.com>
2026-07-16 11:35:56 -0300

    Se dejo fijo el iconó del menu para poder quitar el sidebar a partir de resolución tablet

--- frontend/gestion-de-clases/src/shared/components/side-bar/SideBar.tsx
+++ frontend/gestion-de-clases/src/shared/components/side-bar/SideBar.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import './side-bar.css'
 import pagesData from './pages-data';
 import NavegatorPage from './components/navegator-pages/NavegatorPages';
 import MenuIcon from './components/menu-icon/MenuIcon';
+import './styles/side-bar.css';
 
 const SideBar = () => {
 
-    const [isVisible, setIsVisible] = useState<boolean>(false); 
+    const [isVisible, setIsVisible] = useState(false); 
 
     const onVisible = () => setIsVisible(prevIsVisible => !prevIsVisible);
 
