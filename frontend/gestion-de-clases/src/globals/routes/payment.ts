f12e21de2dfb0d4b9f714b8a0f2d36a0b8e88beb
Nicolás Bossi <nbossi@hotmail.com>
2026-07-16 15:22:30 -0300

    Se terminaron de agregar todas las rutas para el pago

--- frontend/gestion-de-clases/src/globals/routes/payment.ts
+++ frontend/gestion-de-clases/src/globals/routes/payment.ts
@@ -1,1 +1,6 @@
 export const PAYMENT_ROUTE = "payment/*";
+export const MAIN_ROUTE_PAYMENT = "payment/";
+export const REGISTER = "/register";
+export const EXTEND = "/extend";
+export const NEXT_PAYMENT = "/next-payment";
+export const DASHBOARD_PAYMENT = "/dashboard";
--- frontend/gestion-de-clases/src/shared/components/side-bar/pages-data.ts
+++ frontend/gestion-de-clases/src/shared/components/side-bar/pages-data.ts
@@ -1,6 +1,7 @@
-import { ADD, MAIN_ROUTE_FEE } from "@/globals/routes/fee";
+import { ADD, DASHBOARD_FEE, MAIN_ROUTE_FEE } from "@/globals/routes/fee";
 import { ATTENDANCE, DASHBOARD, INCOME, MAIN_ROUTE_STUDENT, UNSUSCRIBE } from "@/globals/routes/student";
 import type { PageRoute } from "./types/page-data";
+import { DASHBOARD_PAYMENT, EXTEND, MAIN_ROUTE_PAYMENT, NEXT_PAYMENT, REGISTER } from "@/globals/routes/payment";
 
 const pagesData: PageRoute[] = [
     {
@@ -9,22 +10,22 @@ const pagesData: PageRoute[] = [
         links: [
             {
                 id: 2,
-                pageName: "Ingreso",
+                pageName: "ingreso",
                 path: `${MAIN_ROUTE_STUDENT + INCOME}`
             },
             {
                 id: 3,
-                pageName: "Asistencia",
+                pageName: "asistencia",
                 path: `${MAIN_ROUTE_STUDENT + ATTENDANCE}`
             },
             {
                 id: 4,
-                pageName: "Consultar",
+                pageName: "consultar",
                 path: `${MAIN_ROUTE_STUDENT + DASHBOARD}`
             },
             {
                 id: 5,
-                pageName: "Dar de Baja",
+                pageName: "dar de Baja",
                 path: `${MAIN_ROUTE_STUDENT + UNSUSCRIBE}`
             }
         ]
@@ -35,13 +36,13 @@ const pagesData: PageRoute[] = [
         links: [
             {
                 id: 6,
-                pageName: "Ingreso",
+                pageName: "ingreso",
                 path: `${MAIN_ROUTE_FEE + ADD}`
             },
             {
                 id: 7,
-                pageName: "Consultar",
-                path: `${MAIN_ROUTE_FEE + DASHBOARD}`
+                pageName: "consultar",
+                path: `${MAIN_ROUTE_FEE + DASHBOARD_FEE}`
             }
         ]
     },
@@ -51,13 +52,28 @@ const pagesData: PageRoute[] = [
         links: [
             {
                 id: 9,
-                pageName: "Ingreso",
-                path: `${MAIN_ROUTE_FEE + ADD}`
+                pageName: "registrar",
+                path: `${MAIN_ROUTE_PAYMENT + REGISTER}`
+            },
+            {
+                id: 10,
+                pageName: "extender",
+                path: `${MAIN_ROUTE_PAYMENT + EXTEND}`
+            },
+            {
+                id: 9,
+                pageName: "siguiente pago",
+                path: `${MAIN_ROUTE_PAYMENT + NEXT_PAYMENT}`
+            },
+            {
+                id: 9,
+                pageName: "dashboard",
+                path: `${MAIN_ROUTE_PAYMENT + DASHBOARD_PAYMENT}`
             }
         ]
     },
     {
-        id: 10,
+        id: 15,
         pageName: "Clases",
         path: `${MAIN_ROUTE_FEE + ADD}`
     },
