86a20a12277a2385515f1f2e21a799566b63f2fb
Nicolás Bossi <nbossi@hotmail.com>
2026-07-28 13:49:02 -0300

    Se modificó la función fresh para que reinicie el estado del error además del dato guardado en el huuk useApi

--- frontend/gestion-de-clases/src/shared/hooks/useAddData.ts
+++ frontend/gestion-de-clases/src/shared/hooks/useAddData.ts
@@ -1,24 +1,24 @@
 import { useState } from "react";
 import useFetch from "./useFetch";
 
-const useAddData = <T, K>(income : (incomer : K) => Promise<T>) => {
-    const [data, setData] = useState<T | null>(null);
+const useAddData = <T, K>(income: (incomer: K) => Promise<T>) => {
+    const [data, setEntity] = useState<T | null>(null);
     const {isLoading, error, freshError, handleFetch} = useFetch();
 
-    const add = async (incomer : K) : Promise<void>=> {
+    const add = async (incomer: K) => {
 
         handleFetch(
-            async () => setData(await income(incomer)),
-            () => setData(null)
+            async () => setEntity(await income(incomer)),
+            () => setEntity(null)
         )
     }
 
-    const freshData = () : void =>  {
-        setData(null);
+    const freshData = () =>  {
+        setEntity(null);
+        freshError()
     }
 
-    return {data, error, freshError, freshData, isLoading, add}
-
+    return {data, error, freshData, isLoading, add}
 }
 
 export default useAddData;
